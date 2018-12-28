d63bc1664eff12f40cd7aeb4ce1a4a3bb997131b
16340206 <31608043+16340206@users.noreply.github.com>
2018-12-28 17:58:58 +0800

    Update Match.sol

--- Match.sol
+++ Match.sol
@@ -2,17 +2,17 @@ pragma solidity ^0.4.2;
 
 contract Match{  
 
-	address public organizer;	//组织者
-	mapping (address => uint) public buyers;	//买票者们
-	uint public count;	//买票人数
-	uint public price;  
-	uint public capacity;	//场馆容量
-	bool public stopsell;	//终止售票
-	bool public soldout;   //票售罄 
+	address public organizer;					//组织者
+	mapping (address => uint) public buyers;	//买票者持有票数
+	uint public count;							//以售出票
+	uint public price;  						//票价
+	uint public capacity;						//场馆容量
+	bool public stopsell;						//终止售票
+	bool public soldout;   						//售票售罄 
 	event Deposit(address _from, uint _amount); // 付款
-	event Refund(address _to, uint _amount); // 退款
+	event Refund(address _to, uint _amount); 	// 退款
 
-	function Match() public{	//构造函数
+	function Match() public{		//构造函数
 		organizer = msg.sender;		
 		stopsell = false;
 		soldout = false;
@@ -21,19 +21,24 @@ contract Match{
 		count = 0;
 	}
 
+	//买票函数
 	function buyTickets (uint amount) public payable{
+		// 票已售罄、买票超过上限、已停止售票、付款不够，退回已支付款项。
 		if (soldout == true || count + amount > capacity 
 		    || stopsell == true || msg.value < (price * amount)) {
 		    msg.sender.transfer(msg.value);
 		    return; 
 		}
+
 		if(msg.value >= (price * amount)) {
-			buyers[msg.sender] += amount;
-			count += amount;
-			if(count == capacity) {
+			buyers[msg.sender] += amount;		//持有票数增加
+			count += amount;					//已售出票增加
+			if(count == capacity) {				//判断票是否售罄
 			    soldout = true;
 			}
 			Deposit(msg.sender, price * amount);
+
+			//返还多余款项
 			uint refundFee = msg.value - price * amount;
 			if(refundFee > 0) {
 				msg.sender.transfer(refundFee);
@@ -47,42 +52,29 @@ contract Match{
 		if (msg.sender != organizer) { return; }
 		if(add > 0) {
 			capacity += add;
-			soldout = false;
+			soldout = false;	//增加容量后存在多余票数
 		}
 	}
-
-
-
-	function refundTicket(uint amount) public payable{	//退票
+	//退票函数
+	function refundTicket(uint amount) public payable{	
 		msg.sender.transfer(msg.value);
-	    if(stopsell == true) { return; }
-	    uint temp = buyers[msg.sender];
-		if (temp >= amount) { 
-			Refund(msg.sender, price * amount);
-			buyers[msg.sender] -= amount;
-			count -= amount;
-			soldout = false;
-			msg.sender.transfer(price * amount);
+	    if(stopsell == true) { return; }			//停止售票后不能再退款
+	    uint temp = buyers[msg.sender];			
+		if (temp >= amount) { 						//申请退票数量小于等于购票数量时才给予处理
+			Refund(msg.sender, price * amount);	
+			buyers[msg.sender] -= amount;			//持有票数减少
+			count -= amount;						//售票总数减少
+			soldout = false;						//存在多余票，没有售罄
+			msg.sender.transfer(price * amount);	//退款
 		}
 		return;
 	}
 
-	/*	哥写报告的时候直接删了吧
-	//	理论上不应该存在这个函数，发布者可以携款而逃，并且再次执行程序引出各种错误，
-	// 	在实际运行中添加这个函数就是犯规的，
-	//	但在测试阶段，执行stopSell之后合约就不能再运行
-	//	每次都要需要重新部署实在是非常麻烦，所以存在这么一个函数。
-	function OverTime() public {				
-	    if (msg.sender == organizer) {
-	        stopsell = false;
-	    }
-	}
-	*/
 
 	function stopSell() public{
-		if (msg.sender == organizer) { // 停止售票
+		if (msg.sender == organizer) {				// 停止售票
 			stopsell = true;
-			organizer.transfer(price * count);
+			organizer.transfer(price * count);		//组织者获得实际收益
 		}
 	}
 }
